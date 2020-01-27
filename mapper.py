9f4fc99411985a3ac2b8ccb6ab3c73703d4da3c1
navymaddox <32476948+navymaddox@users.noreply.github.com>
2020-01-26 20:17:31 -0500

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import sys
+from sklearn.feature_extraction import stop_words
 
-stopwords = set(['the', 'and'])
+stops = set(stop_words.ENGLISH_STOP_WORDS)
 
 # get all lines from stdin
 for line in sys.stdin:
@@ -13,5 +14,5 @@ for line in sys.stdin:
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-	if word not in stopwords:
+	if word not in stops:
 		print '%s\t%s' % (word, "1")

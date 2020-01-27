a8b283b1deabf9a0ca666c875d091a11e688cde9
navymaddox <32476948+navymaddox@users.noreply.github.com>
2020-01-26 20:51:42 -0500

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
-import sys
+import string, sys
 from sklearn.feature_extraction import stop_words
-
-stops = set(stop_words.ENGLISH_STOP_WORDS)
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip().lower()
+    
+    # remove punctuation
+    line = line.translate(string.maketrans(string.punctuation))	
+
+    # make lowercase
+    line = line.lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
+    stops = set(stop_words.ENGLISH_STOP_WORDS)
     for word in words:
 	if word not in stops:
 		print '%s\t%s' % (word, "1")
